c0c79215c8abcd205f7426a09b09473fcc1c941a
Jaya Varsini <jayavarsini007@gmail.com>
2023-07-17 21:19:27 +0530

    register users with username and password

--- app.js
+++ app.js
@@ -2,13 +2,65 @@
 const express = require("express");
 const body_parser = require("body-parser");
 const ejs = require("ejs");
-
+const mongoose = require("mongoose");
 const app = express();
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(body_parser.urlencoded({extended : true}));
 
+mongoose.connect("mongodb://127.0.0.1:27017/userDB", {useNewUrlParser : true});
+
+const userSchema = new mongoose.Schema({
+    email : String,
+    password : String
+});
+
+const User = new mongoose.model("User", userSchema);
+
+app.get("/", function(req, res) {
+    res.render("home");
+});
+
+app.get("/login", function(req, res) {
+    res.render("login");
+});
+
+app.get("/register", function(req, res) {
+    res.render("register");
+});
+
+app.post("/register", function(req, res) {
+    const newUser = new User({
+        email : req.body.username,
+        password : req. body.password
+    });
+
+    newUser.save().then(() => {
+        res.render("secrets");
+    }).catch((err) => {
+        console.log(err);
+    });
+});
+
+app.post("/login", function(req, res) {
+    const username = req.body.username;
+    const passwords = req.body.password;
+
+    User.findOne({email : username}).then((foundUser) => {
+        if(foundUser) {
+            if(foundUser.password === passwords) {
+                res.render("secrets");
+            }else {
+                alert("Incorrect email or password!!");
+            }
+        }else {
+            console.log("User not found.");
+        }
+    }).catch((err) => {
+        console.log(err);
+    });
+});
  
 app.listen(3000, function(req, res) {
     console.log("Server is running in port 3000");
