d17a4dfc4f2273d679ebe3cb9aafa26b0c6c9bf8
Jaya Varsini <jayavarsini007@gmail.com>
2023-07-18 19:21:17 +0530

    Using environmental variables to keep the variables safe

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 //jshint esversion:6
+require("dotenv").config();
 const express = require("express");
 const body_parser = require("body-parser");
 const ejs = require("ejs");
@@ -18,8 +19,7 @@ const userSchema = new mongoose.Schema({
     password : String
 });
 
-secret = "Thisisalittlesecret.";
-userSchema.plugin(encrypt, { secret: secret, encryptedFields : ["password"]});
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields : ["password"]});
 
 const User = new mongoose.model("User", userSchema);
 
